d786583ebf3ddd65519c01d0138b5af429206102
Jason Clement <Jason Clement>
2017-04-30 01:36:54 -0500

    Add project files.

---
+++ Factorio Blueprint Tool/Blueprint.cs
@@ -0,0 +1,123 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using zlib;
+
+namespace Factorio_Blueprint_Tool
+{
+    public class Blueprint
+    {
+        private Blueprint() { }
+
+        public static string Decode(string base64)
+        {
+            if (string.IsNullOrWhiteSpace(base64)) return "";
+            var enc = new UTF8Encoding();
+            return enc.GetString(Decompress(Convert.FromBase64String(base64.Substring(1))));
+        }
+        
+        public static string Encode(string json)
+        {
+            var enc = new UTF8Encoding();
+            return "0" + Convert.ToBase64String(Compress(enc.GetBytes(json)));
+        }
+
+        private static byte[] Compress(byte[] inData)
+        {
+            using (MemoryStream outMemoryStream = new MemoryStream())
+            using (ZOutputStream outZStream = new ZOutputStream(outMemoryStream, zlibConst.Z_DEFAULT_COMPRESSION))
+            using (Stream inMemoryStream = new MemoryStream(inData))
+            {
+                CopyStream(inMemoryStream, outZStream);
+                outZStream.finish();
+                return outMemoryStream.ToArray();
+            }
+        }
+
+        private static byte[] Decompress(byte[] inData)
+        {
+            using (MemoryStream outMemoryStream = new MemoryStream())
+            using (ZOutputStream outZStream = new ZOutputStream(outMemoryStream))
+            using (Stream inMemoryStream = new MemoryStream(inData))
+            {
+                CopyStream(inMemoryStream, outZStream);
+                outZStream.finish();
+                return outMemoryStream.ToArray();
+            }
+        }
+
+        private static void CopyStream(System.IO.Stream input, System.IO.Stream output)
+        {
+            byte[] buffer = new byte[2000];
+            int len;
+            while ((len = input.Read(buffer, 0, 2000)) > 0)
+            {
+                output.Write(buffer, 0, len);
+            }
+            output.Flush();
+        }
+
+        public static string Mirror(string json, bool vertical)
+        {
+            var o = JObject.Parse(json);
+            var entities = o["blueprint"]["entities"];
+            foreach (var entity in entities)
+            {
+                if (entity["direction"] == null)
+                    entity["direction"] = 0;
+                int d = entity["direction"].Value<int>();
+                switch (d)
+                {
+                    case 0: d = vertical ? 4 : 0; break;
+                    case 4: d = vertical ? 0 : 4; break;
+                    case 2: d = vertical ? 2 : 6; break;
+                    case 6: d = vertical ? 6 : 2; break;
+                }
+                entity["direction"] = d;
+
+                if (vertical)
+                {
+                    entity["position"]["y"] = entity["position"]["y"].Value<int>() * -1;
+                }
+                else
+                {
+                    entity["position"]["x"] = entity["position"]["x"].Value<int>() * -1;
+                }
+            }
+            return o.ToString(Formatting.None);
+        }
+
+        public static string ReplaceItems(string json, Dictionary<string, string> itemsToReplace, out string results)
+        {
+            var o = JObject.Parse(json);
+            var entities = o["blueprint"]["entities"];
+            var resultsD = new Dictionary<string, int>();
+            foreach (var entity in entities)
+            {
+                foreach (var key in itemsToReplace.Keys)
+                {
+                    if (entity["name"].Value<string>() == key)
+                    {
+                        entity["name"] = itemsToReplace[key];
+                        if (!resultsD.ContainsKey(key)) resultsD.Add(key, 0);
+                        resultsD[key]++;
+                    }
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The following items were replaced:");
+            sb.AppendLine();
+            foreach (var key in resultsD.Keys)
+            {
+                sb.AppendLine(key + ": " + resultsD[key].ToString());
+            }
+            results = sb.ToString();
+
+            return o.ToString(Formatting.None);
+        }
+
+    }
+}
---
+++ Factorio Blueprint Tool/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Factorio_Blueprint_Tool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PasteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                InputBox.Text = Clipboard.GetText().Trim();
+            }
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(InputBox.Text);
+        }
+
+        private void DecodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OutputBox.Text = Blueprint.Decode(InputBox.Text);
+            }
+            catch (Exception ex)
+            {
+                OutputBox.Text = ex.Message;
+            }
+        }
+
+        private void EncodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                InputBox.Text = Blueprint.Encode(OutputBox.Text);
+            }
+            catch (Exception ex)
+            {
+                InputBox.Text = ex.Message;
+            }
+        }
+
+        private void HMirrorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(InputBox.Text)) return;
+            try
+            {
+                InputBox.Text = Blueprint.Encode(Blueprint.Mirror(Blueprint.Decode(InputBox.Text), false));
+                OutputBox.Text = "Successfully mirrored vertically";
+            }
+            catch (Exception ex)
+            {
+                OutputBox.Text = ex.Message;
+            }
+        }
+
+        private void VMirrorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(InputBox.Text)) return;
+            try
+            {
+                InputBox.Text = Blueprint.Encode(Blueprint.Mirror(Blueprint.Decode(InputBox.Text), true));
+                OutputBox.Text = "Successfully mirrored vertically";
+            }
+            catch (Exception ex)
+            {
+                OutputBox.Text = ex.Message;
+            }
+        }
+
+        private Dictionary<string, Dictionary<string, string>> ItemsToReplace = new Dictionary<string, Dictionary<string, string>>()
+        {
+            { "transport-belt", new Dictionary<string, string>() {
+                { "fast-transport-belt", "transport-belt" },
+                { "express-transport-belt", "transport-belt" },
+                { "fast-underground-belt", "underground-belt" },
+                { "express-underground-belt", "underground-belt" },
+                { "fast-splitter", "splitter" },
+                { "express-splitter", "splitter" }, } },
+            { "fast-transport-belt", new Dictionary<string, string>() {
+                { "transport-belt", "fast-transport-belt" },
+                { "express-transport-belt", "fast-transport-belt" },
+                { "underground-belt", "fast-underground-belt" },
+                { "express-underground-belt", "fast-underground-belt" },
+                { "splitter", "fast-splitter" },
+                { "express-splitter", "fast-splitter" }, } },
+            { "express-transport-belt", new Dictionary<string, string>() {
+                { "transport-belt", "express-transport-belt" },
+                { "fast-transport-belt", "express-transport-belt" },
+                { "underground-belt", "express-underground-belt" },
+                { "fast-underground-belt", "express-underground-belt" },
+                { "splitter", "express-splitter" },
+                { "fast-splitter", "express-splitter" }, } },
+            { "assembling-machine-1", new Dictionary<string, string>() {
+                { "assembling-machine-2", "assembling-machine-1" },
+                { "assembling-machine-3", "assembling-machine-1" }, } },
+            { "assembling-machine-2", new Dictionary<string, string>() {
+                { "assembling-machine-1", "assembling-machine-2" },
+                { "assembling-machine-3", "assembling-machine-2" }, } },
+            { "assembling-machine-3", new Dictionary<string, string>() {
+                { "assembling-machine-1", "assembling-machine-3" },
+                { "assembling-machine-2", "assembling-machine-3" }, } },
+            { "stone-furnace", new Dictionary<string, string>() {
+                { "steel-furnace", "stone-furnace" }, } },
+            { "steel-furnace", new Dictionary<string, string>() {
+                { "stone-furnace", "steel-furnace" }, } },
+            { "inserter", new Dictionary<string, string>() {
+                { "fast-inserter", "inserter" },
+                { "stack-inserter", "inserter" }, } },
+            { "fast-inserter", new Dictionary<string, string>() {
+                { "inserter", "fast-inserter" },
+                { "stack-inserter", "fast-inserter" }, } },
+            { "stack-inserter", new Dictionary<string, string>() {
+                { "inserter", "stack-inserter" },
+                { "fast-inserter", "stack-inserter" }, } },
+            { "filter-inserter", new Dictionary<string, string>() {
+                { "stack-filter-inserter", "filter-inserter" }, } },
+            { "stack-filter-inserter", new Dictionary<string, string>() {
+                { "filter-inserter", "stack-filter-inserter" }, } },
+        };
+
+        private void ItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var item = System.IO.Path.GetFileNameWithoutExtension((button.Content as Image).Source.ToString());
+            try
+            {
+                var s = Blueprint.Decode(InputBox.Text);
+                if (ItemsToReplace.ContainsKey(item))
+                {
+                    string results;
+                    InputBox.Text = Blueprint.Encode(Blueprint.ReplaceItems(s, ItemsToReplace[item], out results));
+                    OutputBox.Text = results;
+                }
+            }
+            catch (Exception ex)
+            {
+                OutputBox.Text = ex.Message;
+            }
+        }
+    }
+}
